10c9d6ba0e0eb95b022305d243cdda1cd90e1ce1
Indrajit Barman <125343258+indrajit002@users.noreply.github.com>
2024-04-16 15:12:45 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,9 +5,7 @@ require('dotenv').config();
 
 
 // Replace this with your MONGOURI.
-// const MONGOURI = "mongodb+srv://bhargavvobilisetti:c711JKv0dPRS0DSn@cluster0.gnkqubb.mongodb.net/DriveReady?retryWrites=true&w=majority";
-// const MONGOURI = "mongodb+srv://indrajitbarman233:Tz2EszNEKx251ITU@cluster01.ax3lkuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01";
-// const MONGOURI="mongodb://localhost:27017/DriveReady"
+
 const MONGOURI = process.env.MONGODB_URI;
 
 const InitiateMongoServer = async () => {
